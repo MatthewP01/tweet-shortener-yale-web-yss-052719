7c7278b7767c6455d737fede51a879877c6dbfda
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 23:12:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def word_substituter(tweet)
   compare_words = dictionary.keys
   tweet.split(" ").map do |shorten|
     if compare_words.include?(shorten)
-      shorten = compare_words[shorten]
+      shorten = dictionary[shorten]
     else
       shorten
     end
