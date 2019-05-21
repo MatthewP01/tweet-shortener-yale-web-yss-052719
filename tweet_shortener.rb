d12740d3315a7c77fecc1c8cbc6ae9bc08818061
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 23:14:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,11 +24,12 @@ def word_substituter(tweet)
   end
   tweet_string.join(" ")
 end
-  tweet.split(" ").map do |word|
-       if dictionary.keys.include?(word.downcase)
-         word = dictionary[word.downcase]
-       else
-         word
-     end
-   end.join(" ")
-  end
+end
+  # tweet.split(" ").map do |word|
+  #      if dictionary.keys.include?(word.downcase)
+  #        word = dictionary[word.downcase]
+  #      else
+  #        word
+  #    end
+  #  end.join(" ")
+  # end
