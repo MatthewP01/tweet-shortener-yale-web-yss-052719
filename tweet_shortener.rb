1eb51cb10aec0732a8216d4cb118daa9cfd7681b
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 23:40:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -50,7 +50,7 @@ end
 
 def shortened_tweet_truncator(tweet_trunc)
     if tweet_trunc.length > 140
-      word_substituter(phrase)[0..140] + "..."
+      word_substituter(tweet_trunc)[0..140] + "..."
     else
       phrase
     end
